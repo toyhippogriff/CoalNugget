448ea29908a67a1343b801471c3b682b2e6eaae7
toyhippogriff <toyhippogriff@gmail.com>
2023-03-18 21:58:30 -0500

    Update to 1.19.2

---
+++ common/src/main/java/toyhippogriff/coalnugget/item/CoalNuggetItems.java
@@ -0,0 +1,40 @@
+package toyhippogriff.coalnugget.item;
+
+import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.fuel.FuelRegistry;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import toyhippogriff.coalnugget.CoalNugget;
+import toyhippogriff.coalnugget.block.CoalNuggetBlocks;
+
+public class CoalNuggetItems
+{
+    public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(CoalNugget.MOD_ID, Registry.ITEM_REGISTRY);
+
+    public static final RegistrySupplier<Item> CHARCOAL_BLOCK = REGISTRY.register("charcoal_block", () ->
+            new BlockItem(CoalNuggetBlocks.CHARCOAL_BLOCK.get(), CoalNuggetItems.properties()));
+    public static final RegistrySupplier<Item> COAL_NUGGET = REGISTRY.register("coal_nugget", () ->
+            new Item(CoalNuggetItems.properties()));
+    public static final RegistrySupplier<Item> CHARCOAL_NUGGET = REGISTRY.register("charcoal_nugget", () ->
+            new Item(CoalNuggetItems.properties()));
+
+    public static CreativeModeTab CREATIVE_TAB = CreativeTabRegistry.create(new ResourceLocation(CoalNugget.MOD_ID, "items"), () ->
+            new ItemStack(COAL_NUGGET.get()));
+
+    public static void init()
+    {
+        FuelRegistry.register(16000, CHARCOAL_BLOCK.get());
+        FuelRegistry.register(200, COAL_NUGGET.get(), CHARCOAL_NUGGET.get());
+    }
+
+    public static Item.Properties properties()
+    {
+        return new Item.Properties().tab(CREATIVE_TAB);
+    }
+}
